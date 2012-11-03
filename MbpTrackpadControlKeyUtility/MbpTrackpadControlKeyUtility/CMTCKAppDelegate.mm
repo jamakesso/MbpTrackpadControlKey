8d03e7327fd60abbc53d8e4379ee5e083b23394b
YAMAOKA Tohru <yamaoka@ss.iij4u.or.jp>
2012-11-03 10:10:43 +0900

    sleep, wake で動作しなくなる不具合修正。

--- MbpTrackpadControlKeyUtility/MbpTrackpadControlKeyUtility/CMTCKAppDelegate.mm
+++ MbpTrackpadControlKeyUtility/MbpTrackpadControlKeyUtility/CMTCKAppDelegate.mm
@@ -31,6 +31,8 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+  // setup status bar.
+  
   NSStatusBar* statusBar = [NSStatusBar systemStatusBar];
   
   _statusItem = [statusBar statusItemWithLength:NSVariableStatusItemLength];
@@ -40,50 +42,8 @@
   [_statusItem setHighlightMode:YES];
   [_statusItem setMenu:_statusMenu];
   
-  io_iterator_t iterator;
-  if (IOServiceGetMatchingServices(kIOMasterPortDefault
-                                   , IOServiceMatching(DRIVER_CLASS_NAME_STRING)
-                                   , &iterator)
-      != KERN_SUCCESS) {
-    NSAlert *alert
-    = [NSAlert alertWithMessageText:@"Error"
-                      defaultButton:@"Abort"
-                    alternateButton:nil
-                        otherButton:nil
-          informativeTextWithFormat:@"Failed to find the service of MbpTrackpadControlKeyDriver.kext."];
-    [alert runModal];
-    [NSApp terminate:self];
-    return;
-  }
-  
-  const io_connect_t nullConnect = io_connect_t();
-  connect_ = nullConnect;
-  io_service_t service;
-  while ((service = IOIteratorNext(iterator)) != IO_OBJECT_NULL) {
-    if (IOServiceOpen(service, mach_task_self(), 0, &connect_) != KERN_SUCCESS) {
-      break;
-    }
-    if (CUserClientFacade().openUserClient(connect_) != KERN_SUCCESS) {
-      break;
-    }
-	}
-  IOObjectRelease(iterator);
-  
-	if (connect_ == nullConnect) {
-    NSAlert *alert
-    = [NSAlert alertWithMessageText:@"Error"
-                      defaultButton:@"Abort"
-                    alternateButton:nil
-                        otherButton:nil
-          informativeTextWithFormat:@"Failed to open the user client of MbpTrackpadControlKeyDriver.kext."];
-    [alert runModal];
-    [NSApp terminate:self];
-    return;
-	}
-  
-//tohru[
-  // parallel desktop 上での動作では touchpad を取得出来ないので以下で test する。
-/*
+  // parallel desktop 上での動作では touchpad を取得出来ないので以下で driver を test する。
+#if 0
   BOOL pressing = NO;
   while (true) {
     pressing = !pressing;
@@ -96,8 +56,9 @@
     [self indicate:pressing];
     sleep(3);
   }
-*/
-//tohru]
+#endif
+  
+  // create trackpad controller.
   
   typedef std::shared_ptr<CTrackpadController> TCSP;
   __weak CMTCKAppDelegate* weakSelf = self;
@@ -105,27 +66,106 @@
   = TCSP(new CTrackpadController
              ([weakSelf](bool isActive) {
                 @autoreleasepool {
-                  [weakSelf indicate:isActive];
+                  [weakSelf handleTrackpadEvent:isActive];
                 }
               }));
+
+  // register sleep, wake notification.
   
-  if (!_trackpadController->initiate()) {
-    NSAlert *alert
-    = [NSAlert alertWithMessageText:@"Error"
-                      defaultButton:@"Abort"
-                    alternateButton:nil
-                        otherButton:nil
-          informativeTextWithFormat:@"Failed to access the trackpad device."];
-    [alert runModal];
+  [self registerSleepWakeNotifications];
+  
+  // start event handling.
+  
+  if (![self initiateTrackpadEventHandling]) {
     [NSApp terminate:self];
-    return;
-  };
+  }
+  
+  return;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
 - (void)applicationWillTerminate:(NSNotification *)notification
 {
+  [self terminateTrackpadEventHandling];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+- (void) receiveSleepNotification: (NSNotification*) notification
+{
+  [self terminateTrackpadEventHandling];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+- (void) receiveWakeNotification: (NSNotification*) notification
+{
+  NSLog(@"receiveWakeNotification: %@", [notification name]);
+  if (![self initiateTrackpadEventHandling]) {
+    [NSApp terminate:self];
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+- (void) registerSleepWakeNotifications
+{
+  [[[NSWorkspace sharedWorkspace] notificationCenter]
+   addObserver: self
+   selector: @selector(receiveSleepNote:)
+   name: NSWorkspaceWillSleepNotification object: NULL];
+  
+  [[[NSWorkspace sharedWorkspace] notificationCenter]
+   addObserver: self
+   selector: @selector(receiveWakeNote:)
+   name: NSWorkspaceDidWakeNotification object: NULL];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+- (BOOL)initiateTrackpadEventHandling
+{
+  io_iterator_t iterator;
+  if (IOServiceGetMatchingServices(kIOMasterPortDefault
+                                   , IOServiceMatching(DRIVER_CLASS_NAME_STRING)
+                                   , &iterator)
+      != KERN_SUCCESS) {
+    [self alertMessage:@"Failed to find the service of MbpTrackpadControlKeyDriver.kext."];
+    return NO;
+  }
+  
+  connect_ = IO_OBJECT_NULL;
+  io_service_t service;
+  while ((service = IOIteratorNext(iterator)) != IO_OBJECT_NULL) {
+    if (IOServiceOpen(service, mach_task_self(), 0, &connect_) != KERN_SUCCESS) {
+      break;
+    }
+    if (CUserClientFacade().openUserClient(connect_) != KERN_SUCCESS) {
+      break;
+    }
+	}
+  IOObjectRelease(iterator);
+  
+	if (connect_ == IO_OBJECT_NULL) {
+    [self alertMessage:@"Failed to open the user client of MbpTrackpadControlKeyDriver.kext."];
+    return NO;
+	}
+  
+  if (!_trackpadController->initiate()) {
+    [self alertMessage:@"Failed to access the trackpad device."];
+    return NO;
+  }
+  
+  return YES;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+- (void)terminateTrackpadEventHandling
+{
+  _trackpadController->terminate();
+  
   if (connect_ == IO_OBJECT_NULL) {
     return;
 	}
@@ -133,11 +173,12 @@
   kern_return_t kr1 = IOServiceClose(connect_);
   assert(kr0 == KERN_SUCCESS);
   assert(kr1 == KERN_SUCCESS);
+  connect_ = IO_OBJECT_NULL;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-- (void)indicate:(BOOL)isPressed
+- (void)handleTrackpadEvent:(BOOL)isPressed
 {
   if (connect_ == IO_OBJECT_NULL) {
     return;
@@ -152,6 +193,19 @@
   
   const kern_return_t kr = CUserClientFacade().setControlModifierKey(connect_, isPressed);
   assert(kr == KERN_SUCCESS);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+- (void)alertMessage:(NSString*)message
+{
+  NSAlert *alert
+  = [NSAlert alertWithMessageText:@"Error"
+                    defaultButton:@"Abort"
+                  alternateButton:nil
+                      otherButton:nil
+        informativeTextWithFormat:message];
+  [alert runModal];
 }
 
 @end
